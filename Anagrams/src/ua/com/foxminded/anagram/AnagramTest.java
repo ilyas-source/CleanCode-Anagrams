da8b76cb63a1236c73384ccff7c46955bf5211ce
Ilyas Saitov <saitov1981@gmail.com>
2021-02-16 12:53:22 +0300

    Junit 2

--- Anagrams/src/ua/com/foxminded/anagram/AnagramTest.java
+++ Anagrams/src/ua/com/foxminded/anagram/AnagramTest.java
@@ -6,18 +6,46 @@ import org.junit.jupiter.api.Test;
 
 class AnagramTest {
 
+	Anagram anagram = new Anagram();
+
 	@Test
-	void testReverseText() {
-		Anagram tester = new Anagram();
-		assertEquals(null, tester.reverseText(null));
-		assertEquals("", tester.reverseText(""));
-		assertEquals("abcd qwer", tester.reverseText("dcba rewq"));
-		assertEquals("123 321", tester.reverseText("123 321"));
-		assertEquals("!@#$ %^&*", tester.reverseText("!@#$ %^&*"));
-		assertEquals("qwe1234rty 9876asdf", tester.reverseText("ytr1234ewq 9876fdsa"));
-		assertEquals("qwe!@#$rty %^&*asdf", tester.reverseText("ytr!@#$ewq %^&*fdsa"));
-		assertEquals("123!@#$456 %^&*9876", tester.reverseText("123!@#$456 %^&*9876"));
-		assertEquals("123!@#$4ab qw&*9876", tester.reverseText("123!@#$4ba wq&*9876"));
+	void givenNull_onReverseText_getNull() {
+		assertEquals(null, anagram.reverseText(null));
+	}
+
+	@Test
+	void givenEmptyString_onReverseText_getEmptyString() {
+		assertEquals("", anagram.reverseText(""));
+	}
+
+	@Test
+	void givenPlainWord_onReverseText_getReversedWord() {
+		assertEquals("Hello", anagram.reverseText("olleH"));
+	}
+
+	@Test
+	void givenMixedWord_onReverseText_getReversedWord() {
+		assertEquals("qwe1234rty", anagram.reverseText("ytr1234ewq"));
+	}
+
+	@Test
+	void givenNoLettersWord_onReverseText_getSameWord() {
+		assertEquals("123!@#$456", anagram.reverseText("123!@#$456"));
+	}
+
+	@Test
+	void givenPlainText_onReverseText_getReversedText() {
+		assertEquals("Hello World", anagram.reverseText("olleH dlroW"));
+	}
+
+	@Test
+	void givenMixedText_onReverseText_getReversedText() {
+		assertEquals("qwe!@#$rt1 %^&*asdf", anagram.reverseText("tre!@#$wq1 %^&*fdsa"));
+	}
+
+	@Test
+	void givenNoLettersText_onReverseText_getSameText() {
+		assertEquals("123!@#$4 &*9876", anagram.reverseText("123!@#$4 &*9876"));
 	}
 
 }
